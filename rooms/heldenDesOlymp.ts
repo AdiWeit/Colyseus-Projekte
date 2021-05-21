35a23c52a08b5b0300e3fa8a2e083c9bea68a5b1
AdiWeit <adi004@web.de>
2021-05-21 18:41:12 +0200

    cards added

--- rooms/heldenDesOlymp.ts
+++ rooms/heldenDesOlymp.ts
@@ -38,6 +38,7 @@ export class heldenDesOlympRoom extends Room {
         [2, ["blue", "blue"], "blue", 1, 3, "Wenn Hylas ins Spiel kommt, ziehe 2 Karten. ", "Held/-in"],
         [3, ["red", "green", "green"], "green", 1, 3, "Wenn Neoptolemos ins Spiel kommt, kannst du bis zu 2 Scha- den auf ein- em ei- ge- nen Hel- den hei- len. ", "Held/-in"],
         [1, ["yellow"], "yellow", 2, 2, "Wenn Alkmene ins Spiel kommt, zieht dein Gegner 1 Karte. ", "Held/-in"],
+        [1, ["yellow"], "yellow", 2, 2, "Wenn Alkmene ins Spiel kommt, zieht dein Gegner 1 Karte. ", "Held/-in"],
         [2, ["red", "blue"], "red", 0, 3, "1 Ein- heit auf ei- nem zu Kyknos be- nach- bar- ten Platz kann bis zu 2X pro Runde an- grei- fen. ", "Held/-in"],
         [2, ["blue", "yellow"], "blue", 2, 1, "Wenn Orpheus ins Spiel kommt, kannst du eine geg- ne- ri- sche Ein- heit be- stim- men, die ma- xi- mal 2 Kar- ten kos- tet. Dein Geg- ner muss sie auf die Hand zur- rück- neh- men. ", "Held/-in"],
         [3, ["red", "green", "blue"], "red", 1, 4, "Wenn Kassandra ins Spiel kommt, fügt sie al- len Ein- hei- ten 1 Scha- den zu (auch ei- ge- nen). ", "Held/-in"],
@@ -94,13 +95,33 @@ export class heldenDesOlympRoom extends Room {
         /*  [],
           [], */
 
+        [2, ["blue", "green"], "green", 1, 3, "Wenn die Danaer ins Spiel kom- men, kannst du 1 Scha- den auf ei- ner ei- ge- nen Ein- heit hei- len. ", "Held/-in"],
+        [3, ["green", "red", "blue"], "blue", "red", 1, 3, "Immer wenn Pollux ei- nen Hel- den an-greift, zie- he 1 Kar- te. ", "Held/-in"],
+        [3, ["blue", "red", "red"], "red", 2, 3, "Wenn Aeneas ins Spiel kommt, kannst du ein- nen Sol- da- ten dei- nes Geg- ners auf den Ab- la- ge- sta- pel le- gen. ", "Held/-in"],
+        [3, ["green", "yellow", "yellow"], "yellow", 0, 4, "Immer wenn Cheiron an- greift, kann er vor- her einer belie- bi- gen geg- ne- ri- chen Ein- hei 1 Scha- den zu- fü- gen. ", "Held/-in"],
+        [4, ["yellow", "red", "blue", "green"], "green", 1, 3, "Immer wenn Elektra an- greift. kannst du vor- her 1 Scha- den von ei- ner ei- ge- nen Ein- heit hei- len. ", "Held/-in"],
+        [3, ["green", "yellow", "blue"], "blue", 1, 2, "Wenn Telamon ins Spiel kommt, ziehe für jeden aus- lie- gen- den geg- ne- ri- schen Hel- den 1 Karte. ", "Held/-in"],
+        [2, ["yellow", "yellow"], "yellow", 1, 3, "Jeglicher Schaden, der Euphemos vo gel- ben Kar- ten zu- ge- fügt wür- de, wird ver- hin- dert. ", "Held/-in"],
+        [2, ["red", "yellow"], "yellow", 1, 3, "Jeglicher Schaden, der Mopsos von ro- ten Kar- ten zu- ge- fügt würde, wird ver- hin- dert. "],
+        [2, ["green", "yellow"], "yellow", 1, 3, "Jeglicher Schaden, der Ariadne von grü- nen Kar- ten zu- ge- fügt wer- den wür- de, wird ver- hin- dert. "],
+        [2, ["blue", "yellow"], "yellow", 1, 3, "Jeglicher Scha- den, der Atalante von blau- en Kar- ten zu- ge- fügt wür- de, wird ver- hin- dert. ", "Held/-in"],
+        [2, ["green", "red"], "red", 1, 3, "Immer wenn Andromache an- greift und ei- ne grü- ne Ein- heit als Geg- ner hat, er- höht sich ihr An- griffs- wert um 1 bis zum En- de des An- griffs. ", "Held/-in"],
+
+        [2, ["red", "blue"], "blue", 3, 2, "Wenn Lynkeus ins Spiel kommt, kann er in die- ser Run- de nicht an- grei- fen. ", "Held/-in"],
+        [3, ["red", "yellow", "blue"], "blue", 1, 3, "Immer wenn Kastor ei- nen Sol- da- ten an- greift, zie- he 1 Kar- te. ", "Held/-in"],
+        [2, ["green", "blue"], "blue", 1, 3, "Immer wenn die melagros ge- gen- ü- ber- lie- gen- de Ein- heit ab- ge- legt wird, zie- he 1 Karte. ", "Held/-in"],
+        [3, ["red", "yellow"], "yellow", 1, 3, "Immer wenn Iolaos an- greift, zieht dein geg- ner 1 Karte. ", "Held/-in"],
+        [3, ["red", "yellow", "green"], "green", 1, 4, "Wenn Patroklos ins Spiel kommt, fügt er je- der ro- ten Ein- heit 1 Scha- den zu (auch ei- ge- nen). ", "Held/-in"],
+        [2, ["blue", "yellow", "red"], "red", 3, 1, "Lege Laodike am En- de dei- nes Spiel- zuges auf den Ab- la- ge- stapel. ", "Held/-in"],
+    //    [3, ["red", "blue", "blue"], "blue", 1, 4, "Wenn Peleus ins Spiel kommt, wählt dein Geg- ner ei- ne sei- ner Hand- karten und gibt sie dir. Spielse sie sofort kos- ten- los aus oder gib ihm zurück. ", "Held/-in"],
+
        [5, ["green", "green", "yellow", "red", "blue"], "silver", 3, 5, "Solange Ajax im Spiel ist, ziehst du in Pha- se 3 im- mer eine Kar- te mehr. ", "Held/-in" ],
        [4, ["red", "red", "green", "blue"], "silver", 1, 4, "Wenn Perseus ins Spiel kommt, kannst du ei- ne aus- lie- gen- de Ein- heit dei- nes Geg- ners auf den Ab- la- ge- sta- pel le- gen. ", "Held/-in" ],
        [4, ["green", "red", "yellow", "yellow"], "silver", 2, 5, "Nachdem Helena an- ge- grif- fen hat, darfst du sie auf ei- nen frei- en Platz auf dei- ner Sei- te ver- schie- ben. ", "Held/-in" ],
        [4, ["yellow", "blue", "red", "red"], "silver", 1, 5, "Wenn Paris ins Spiel kommt, fügt er allen geg- ne- ri- schen Ein- hei- ten 2 Scha- den und allen ei- ge- nen Ein- heit- tein 1 Scha- den zu. ", "Held/-in" ],
       /* check */ [4, ["yellow", "blue", "green", "green"], "silver", 1, 4, "Solange Achilleus im Spiel ist, ha- ben al- le dei- ne Hel- den +1 An- griffs- wert. ", "Held/-in" ],
       /* check  [5, ["blue", "green", "red", "yellow", "yellow"], "silver", 2, 5, "Wenn Herakles an- greift, kann er vor- her zu- sätz- lich ei- ner Ein- heit 2 Scha- den zu- fü- gen. ", "Held/-in"], */
-      /* check */  [5, ["blue", "green", "yellow", "red", "red"], "silver", 1, 6, "Immer wenn Hektor an- greift, kann er vor- her zu- sätz- lich je- der Ein- heit dei- nes Geg- ners 1 Scha- den zu- fü- gen. " ]
+      /* check */  [5, ["blue", "green", "yellow", "red", "red"], "silver", 1, 6, "Immer wenn Hektor an- greift, kann er vor- her zu- sätz- lich je- der Ein- heit dei- nes Geg- ners 1 Scha- den zu- fü- gen. ", "Held/-in" ]
       /* check  [],
       /* check  [],
       /* check  [],
